93a914ab8afc003018f9aa5dd36e29e5b45996d7
吴胜 <pickjob@126.com>
2020-06-23 15:28:05 +0800

    ikonli 学习

---
+++ application/src/main/java/fx/ikonli/IkonliShowCase.java
@@ -0,0 +1,145 @@
+package fx.ikonli;
+
+import com.google.common.collect.ImmutableList;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import org.kordamp.ikonli.Ikon;
+import org.kordamp.ikonli.dashicons.Dashicons;
+import org.kordamp.ikonli.devicons.Devicons;
+import org.kordamp.ikonli.elusive.Elusive;
+import org.kordamp.ikonli.entypo.Entypo;
+import org.kordamp.ikonli.feather.Feather;
+import org.kordamp.ikonli.fontawesome.FontAwesome;
+import org.kordamp.ikonli.fontawesome5.FontAwesomeBrands;
+import org.kordamp.ikonli.fontawesome5.FontAwesomeRegular;
+import org.kordamp.ikonli.fontawesome5.FontAwesomeSolid;
+import org.kordamp.ikonli.fontelico.Fontelico;
+import org.kordamp.ikonli.foundation.Foundation;
+import org.kordamp.ikonli.hawconsfilled.HawconsFilled;
+import org.kordamp.ikonli.hawconsstroke.HawconsStroke;
+import org.kordamp.ikonli.icomoon.Icomoon;
+import org.kordamp.ikonli.ionicons.Ionicons;
+import org.kordamp.ikonli.ionicons4.Ionicons4IOS;
+import org.kordamp.ikonli.ionicons4.Ionicons4Logo;
+import org.kordamp.ikonli.ionicons4.Ionicons4Material;
+import org.kordamp.ikonli.javafx.FontIcon;
+import org.kordamp.ikonli.ligaturesymbols.LigatureSymbols;
+import org.kordamp.ikonli.linecons.Linecons;
+import org.kordamp.ikonli.maki.Maki;
+import org.kordamp.ikonli.maki2.Maki2;
+import org.kordamp.ikonli.mapicons.Mapicons;
+import org.kordamp.ikonli.material.Material;
+import org.kordamp.ikonli.materialdesign.MaterialDesign;
+import org.kordamp.ikonli.metrizeicons.MetrizeIcons;
+import org.kordamp.ikonli.ociicons.Ociicons;
+import org.kordamp.ikonli.octicons.Octicons;
+import org.kordamp.ikonli.openiconic.Openiconic;
+import org.kordamp.ikonli.paymentfont.PaymentFont;
+import org.kordamp.ikonli.runestroicons.Runestroicons;
+import org.kordamp.ikonli.themify.Themify;
+import org.kordamp.ikonli.typicons.Typicons;
+import org.kordamp.ikonli.weathericons.WeatherIcons;
+import org.kordamp.ikonli.websymbols.Websymbols;
+import org.kordamp.ikonli.zondicons.Zondicons;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+
+/**
+ * @author: pickjob@126.com
+ * @time: 2020-06-23
+ **/
+public class IkonliShowCase extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        List<Class<? extends Enum>> list = Arrays.asList(
+                Dashicons.class
+                , Devicons.class
+                , Elusive.class
+                , Entypo.class
+                , Feather.class
+                , FontAwesome.class
+                , FontAwesomeBrands.class
+                , FontAwesomeSolid.class
+                , FontAwesomeRegular.class
+                , Fontelico.class
+                , Foundation.class
+                , HawconsFilled.class
+                , HawconsStroke.class
+                , Icomoon.class
+                , Ionicons4Material.class
+                , Ionicons4IOS.class
+                , Ionicons4Logo.class
+                , Ionicons.class
+                , LigatureSymbols.class
+                , Linecons.class
+                , Maki.class
+                , Maki2.class
+                , Mapicons.class
+                , Material.class
+                , MaterialDesign.class
+                , MetrizeIcons.class
+                , Ociicons.class
+                , Octicons.class
+                , Openiconic.class
+                , PaymentFont.class
+                , Runestroicons.class
+                , Themify.class
+                , Typicons.class
+                , WeatherIcons.class
+                , Websymbols.class
+                , Zondicons.class
+
+        );
+        TabPane tabPane = new TabPane();
+        for (Class<? extends Enum> cls : list) {
+            GridPane gridPane = new GridPane();
+            gridPane.setHgap(10);
+            gridPane.setVgap(10);
+            gridPane.setPadding(new Insets(10));
+            int iconIdx = 0;
+            for (Object val : EnumSet.allOf(cls)) {
+                Ikon ikon = (Ikon) val;
+                VBox vBox = new VBox();
+                FontIcon icon = FontIcon.of(ikon, 40);
+                Label label = new Label(ikon.getDescription());
+                vBox.getChildren().addAll(icon, label);
+                gridPane.add(vBox, iconIdx % 20, iconIdx / 20);
+                iconIdx++;
+            }
+            ScrollPane scrollPane = new ScrollPane(gridPane);
+            Tab tab = new Tab();
+            tab.setText(cls.getSimpleName());
+            tab.setContent(scrollPane);
+            tab.setClosable(false);
+            tabPane.getTabs().add(tab);
+        }
+        Scene scene = new Scene(tabPane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("ikonli JavaFx icon show case");
+
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        primaryStage.setX(bounds.getWidth() / 4);
+        primaryStage.setY(bounds.getHeight() / 4);
+        primaryStage.setWidth(bounds.getWidth() / 2);
+        primaryStage.setHeight(bounds.getHeight() / 2);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- application/src/main/java/module-info.java
+++ application/src/main/java/module-info.java
@@ -30,6 +30,41 @@ module pickjob.java.starter {
     requires javafx.swing;
     requires javafx.web;
     requires javafx.media;
+    // ikonli javafx icon packs
+    requires org.kordamp.iconli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.dashicons;
+    requires org.kordamp.ikonli.devicons;
+    requires org.kordamp.ikonli.elusive;
+    requires org.kordamp.ikonli.entypo;
+    requires org.kordamp.ikonli.feather;
+    requires org.kordamp.ikonli.fontawesome;
+    requires org.kordamp.ikonli.fontawesome5;
+    requires org.kordamp.ikonli.fontelico;
+    requires org.kordamp.ikonli.foundation;
+    requires org.kordamp.ikonli.hawconsfilled;
+    requires org.kordamp.ikonli.hawconsstroke;
+    requires org.kordamp.ikonli.icomoon;
+    requires org.kordamp.ikonli.ionicons;
+    requires org.kordamp.ikonli.ionicons4;
+    requires org.kordamp.ikonli.ligaturesymbols;
+    requires org.kordamp.ikonli.linecons;
+    requires org.kordamp.ikonli.maki;
+    requires org.kordamp.ikonli.maki2;
+    requires org.kordamp.ikonli.mapicons;
+    requires org.kordamp.ikonli.material;
+    requires org.kordamp.ikonli.materialdesign;
+    requires org.kordamp.ikonli.metrizeicons;
+    requires org.kordamp.ikonli.ociicons;
+    requires org.kordamp.ikonli.octicons;
+    requires org.kordamp.ikonli.openiconic;
+    requires org.kordamp.ikonli.paymentfont;
+    requires org.kordamp.ikonli.runestroicons;
+    requires org.kordamp.ikonli.themify;
+    requires org.kordamp.ikonli.typicons;
+    requires org.kordamp.ikonli.weathericons;
+    requires org.kordamp.ikonli.websymbols;
+    requires org.kordamp.ikonli.zondicons;
 
     // exports
     opens basic.rmi.service to java.rmi;
@@ -58,5 +93,7 @@ module pickjob.java.starter {
     opens fx.charts to javafx.base
             , javafx.graphics
             , javafx.fxml
+            ;
+    opens fx.ikonli to javafx.graphics
             ;
 }
