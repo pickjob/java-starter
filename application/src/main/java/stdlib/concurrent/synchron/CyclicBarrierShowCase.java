842913ac238e79d894e271d121c669692fd3d818
吴胜 <pickjob@126.com>
2022-12-13 14:35:48 +0800

    Concurrent Synchronizer

---
+++ application/src/main/java/stdlib/concurrent/synchron/CyclicBarrierShowCase.java
@@ -0,0 +1,42 @@
+package stdlib.concurrent.synchron;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * CyclicBarrier: 用于控制多个线程同时执行同步器
+ *      await(): 等待所有线程到达指定位置并等待
+ *      reset(): 重置同步器
+ *
+ * @author: pickjob@126.com
+ * @date: 2022-12-13
+ */
+public class CyclicBarrierShowCase {
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.info("CyclicBarrier: 用于控制多个线程同时执行同步器");
+        int parties = 10;
+        final CyclicBarrier cyclicBarrier = new CyclicBarrier(parties);
+        for (int i = 0; i < parties; i++) {
+            new Thread(() -> {
+                try {
+                    int sleepMilliseconds = (int) (Math.random() * 10 * 1000); // 随机生成10秒以内毫秒数
+                    logger.info("Thread: {}, sleepMilliseconds: {}", Thread.currentThread(), sleepMilliseconds);
+                    Thread.sleep(sleepMilliseconds);
+                    cyclicBarrier.await();
+                    logger.info("Thread: {} execute", Thread.currentThread().getName());
+                } catch (Exception e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }).start();
+        }
+        try {
+            Thread.sleep(20 * 1000);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+}
---
+++ application/src/main/java/stdlib/concurrent/synchron/ReentrantReadWriteLockShowCase.java
@@ -0,0 +1,76 @@
+package stdlib.concurrent.synchron;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * ReentrantReadWriteLock: 读写锁, 读共享, 写排他(包括读)
+ *
+ * @author pickjob@126.com
+ * @time 2022-12-13
+ */
+public class ReentrantReadWriteLockShowCase {
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.info("ReentrantReadWriteLock: 读写锁, 读共享, 写排他(包括读)");
+        int num = 10;
+        CountDownLatch countDownLatch = new CountDownLatch(num);
+        RWDictionary dictionary = new RWDictionary();
+        for (int i = 0; i < num; i++ ) {
+            final int idx = i;
+            new Thread(() -> {
+                dictionary.put("" + idx, idx);
+                countDownLatch.countDown();
+            }).start();
+        }
+        try {
+            countDownLatch.await();
+            for (int i = 0; i < num; i++ ) {
+                final int idx = i;
+                new Thread(() -> {
+                    logger.info("{}: {}", idx, dictionary.get("" + idx));
+                }).start();
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+}
+
+class RWDictionary {
+    private final Map<String, Object> m = new TreeMap<>();
+    private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+    private final Lock r = rwl.readLock();
+    private final Lock w = rwl.writeLock();
+
+    public Object get(String key) {
+        r.lock();
+        try { return m.get(key); }
+        finally { r.unlock(); }
+    }
+
+    public Object put(String key, Object value) {
+        w.lock();
+        try {
+            return m.put(key, value);
+        } finally {
+            w.unlock();
+        }
+    }
+
+    public void clear() {
+        w.lock();
+        try {
+            m.clear();
+        } finally {
+            w.unlock();
+        }
+    }
+}
